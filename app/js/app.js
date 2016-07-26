04d9d595ad65f431b1a4ef9c05eb192b14e2e96c
lakhansamani <lakhan.m.samani@gmail.com>
2016-07-26 15:38:11 +0530

    Adds wrapper and home module with ui routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,31 @@
 (function(){
 	'use strict';
-	angular.module('myApp',['ui.router','ngMaterial']);
+	angular.module('myApp',['ui.router','ngMaterial'])
+	.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+		.state('main',{
+			views:{
+				'@':{
+					templateUrl: 'modules/wrapper/layout.html'
+				},
+				'top@main':{
+					templateUrl: 'modules/wrapper/menu.html',
+				},
+				'content@main':{
+					templateUrl: 'modules/wrapper/wrap.html'
+				}
+			}
+
+		})
+		.state('main.home',{
+			url:'/home',
+			views:{
+				'mainSection@main':{
+					templateUrl : 'modules/home/home.html'
+				}
+			}
+		});
+		$urlRouterProvider.otherwise('/home');
+	}]);
+
 })();
